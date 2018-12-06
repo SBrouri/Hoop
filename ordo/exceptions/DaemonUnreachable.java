a8d104d77ea7ae7968875fa6eacf363a3a9d0c7a
SBrouri <soufian.abrouri@gmail.com>
2018-12-06 15:34:07 +0100

    rm old excep

--- ordo/exceptions/DaemonUnreachable.java
+++
@@ -1,9 +0,0 @@
-package ordo.exceptions;
-
-// This exception is about connexion with the daemon (daemon_Id)
-
-public class DaemonUnreachable extends HidoopFail{
-  public DaemonUnreachable(String daemonId) {
-    super("Can't contact the damon" + daemonId);
-  }
-}
